f11c2c23ca6cbd8cf77b83247ea88bb1f3e7a225
Niklas Donocik <n.donocik@tu-bs.de>
2018-08-23 15:15:27 +0200

    Updated scripts

---
+++ build_gcc.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+## Dependencies gcc g++ make m4 gcc-multilib autogen
+
+## Setup
+#prefix=$PWD
+prefix=/home/niklas/gcc # Directory to use
+proc_make=4		# Processes to use for compiling (usually 1.5...2
+			#                                 number of processors)
+gmp_ver=6.1.2		# Version of GNU MP to download 
+			# ! check url and filename in script for changes !
+mpc_ver=1.1.0		# Version of GNU MPC to download 
+			# ! check url and filename in script for changes !
+mpfr_ver=4.0.1		# Version of GNU MPC to download 
+			# ! check url and filename in script for changes !
+gcc_ver=7.3.0		# Version of GNU MPC to download 
+			# ! check url and filename in script for changes !
+
+printf "Prepare directory ....................................................\
+..... "
+
+## Remove earlier attempts
+rm -rf $prefix/log
+rm -f *.tar.bz2
+rm -f *.tar.gz
+
+rm -rf $prefix/gmp
+rm -rf gmp-$gmp_ver
+
+rm -rf $prefix/mpfr
+rm -rf mpfr-$mpc_ver
+
+rm -rf $prefix/mpc
+rm -rf mpc-$mpc_ver
+
+rm -rf $prefix/gcc
+rm -rf gcc-$gcc_ver
+
+## Make log directory
+mkdir $prefix/log
+
+printf "done\n"
+
+## Build GMP
+printf "Downloading GNU MP version %s ........................................\
+.. " "$gmp_ver"
+(wget https://gmplib.org/download/gmp/gmp-$gmp_ver.tar.bz2
+tar xf gmp-$gmp_ver.tar.bz2) > $prefix/log/gmp-0_download.log 2>&1
+rm gmp-$gmp_ver.tar.bz2
+cd gmp-$gmp_ver
+printf "done\n"
+printf "Configuring ........ "
+(./configure --prefix=$prefix/gmp) > $prefix/log/gmp-1_conf.log 2>&1
+printf "building ........ "
+(make -j $proc_make) > $prefix/log/gmp-2_make.log 2>&1
+printf "checking ........ "
+(make check -j $proc_make) > $prefix/log/gmp-3_check.log 2>&1
+printf "installing ....... "
+(make install) > $prefix/log/gmp-4_install.log 2>&1
+printf "done\n"
+cd ..
+rm -rf gmp-$gmp_ver
+
+## Build MPFR
+printf "Downloading GNU MPFR version %s ......................................\
+.. " "$mpfr_ver"
+(wget https://www.mpfr.org/mpfr-current/mpfr-$mpfr_ver.tar.gz
+tar xf mpfr-$mpfr_ver.tar.gz) > $prefix/log/mpfr-0_download.log 2>&1
+rm mpfr-$mpfr_ver.tar.gz
+cd mpfr-$mpfr_ver
+printf "done\n"
+printf "Configuring ........ "
+(./configure --prefix=$prefix/mpfr --with-gmp=$prefix/gmp
+) > $prefix/log/mpfr-1_conf.log 2>&1
+printf "building ........ "
+(make -j $proc_make) > $prefix/log/mpfr-2_make.log 2>&1
+printf "checking ........ "
+(make check -j $proc_make) > $prefix/log/mpfr-3_check.log 2>&1
+printf "installing ....... "
+(make install) > $prefix/log/mpfr-4_install.log 2>&1
+printf "done\n"
+cd ..
+rm -rf mpfr-$mpfr_ver
+
+## Build MPC
+printf "Downloading GNU MPC version %s .......................................\
+.. " "$mpc_ver"
+(wget https://ftp.gnu.org/gnu/mpc/mpc-$mpc_ver.tar.gz
+tar xf mpc-$mpc_ver.tar.gz) > $prefix/log/mpc-0_download.log 2>&1
+rm mpc-$mpc_ver.tar.gz
+cd mpc-$mpc_ver
+printf "done\n"
+printf "Configuring ........ "
+(./configure --prefix=$prefix/mpc --with-gmp=$prefix/gmp \
+--with-mpfr=$prefix/mpfr) > $prefix/log/mpc-1_conf.log 2>&1
+printf "building ........ "
+(make -j $proc_make) > $prefix/log/mpc-2_make.log 2>&1
+printf "checking ........ "
+(make check -j $proc_make) > $prefix/log/mpc-3_check.log 2>&1
+printf "installing ....... "
+(make install) > $prefix/log/mpc-4_install.log 2>&1
+printf "done\n"
+cd ..
+rm -rf mpc-$mpc_ver
+
+## Export those libraries, so gcc can find them (even though we pass their path
+##  as options)
+
+export LD_LIBRARY_PATH=$prefix/gmp/lib:$prefix/mpc/lib:$prefix/mpfr/lib
+
+## Build gcc
+printf "Downloading GNU GCC version %s .......................................\
+.. " "$gcc_ver"
+(wget ftp://ftp.fu-berlin.de/unix/languages/gcc/releases/gcc-$gcc_ver/\
+gcc-$gcc_ver.tar.gz
+tar xf gcc-$gcc_ver.tar.gz) > $prefix/log/gcc-0_download.log 2>&1
+rm gcc-$gcc_ver.tar.gz
+cd gcc-$gcc_ver
+printf "done\n"
+printf "Configuring ........ "
+(./configure\
+  --prefix=$prefix/gcc\
+  --with-gmp=$prefix/gmp\
+  --with-mpfr=$prefix/mpfr\
+  --with-mpc=$prefix/mpc\
+  --program-suffix=-$gcc_ver\
+  --enable-languages=c,c++\
+ ) > $prefix/log/gcc-1_conf.log 2>&1
+printf "building ........ "
+(make -j $proc_make) > $prefix/log/gcc-2_make.log 2>&1
+printf "checking ........ "
+(make check -j $proc_make) > $prefix/log/gcc-3_check.log 2>&1
+printf "installing ....... "
+(make install) > $prefix/log/gcc-4_install.log 2>&1
+printf "done\n"
+cd ..
+rm -rf gcc-$gcc_ver
--- siloutil-setup.sh
+++ siloutil-setup.sh
@@ -1,66 +1,142 @@
 #!/bin/bash
 
+## Dependencies git python python-dev
+
+## Setup
+prefix=/home/niklas/siloutil 	# Directory to use
+proc_make=4			# Processes to use for compiling
+				# (usually 1.5...2 number of processors)
+gcc_dir=/home/niklas/gcc	# $prefix from script build_gcc.sh
+gcc_suff=-7.3.0			# suffix from script build_gcc.sh (-X.Y.Z.)
+
+
+## Remove earlier attempts
 rm -rf zlib
 rm -rf hdf5
 rm -rf silo
-rm -rf binaries
+rm -rf boost
+rm -rf $prefix/thirdparty
 
-export CC=gcc-8.2
-export CXX=g++-8.2
+mkdir $prefix/thirdparty
+mkdir $prefix/thirdparty/log
+
+export PATH=/home/niklas/gcc/gcc/bin:$PATH
+export AR=$gcc_dir/gcc/bin/gcc-ar$gcc_suff
+export CC=$gcc_dir/gcc/bin/gcc$gcc_suff
+export CXXCPP="$gcc_dir/gcc/bin/g++$gcc_suff -E"
+export CPP="$gcc_dir/gcc/bin/g++$gcc_suff -E"
+export CXX=$gcc_dir/gcc/bin/g++$gcc_suff
 export CFLAGS="-fPIC"
-export LD_LIBRARY_PATH=/home/niklas/gcc/lib:/home/niklas/gcc/gmp/lib:/home/niklas/gcc/mpc/lib:/home/niklas/gcc/mpfr/lib
+export LD_LIBRARY_PATH=$gcc_dir/gcc/lib:$gcc_dir/gmp/lib:$gcc_dir/mpc/lib:\
+$gcc_dir/mpfr/lib
 export LIBS="-ldl"
 export HDF5TestExpress=0
 
-mkdir binaries
-
-prefix=$PWD
-logfile=setup.log
-
-(
-git clone https://github.com/siloutil/zlib
+## Building zlib
+printf "Downloading Zlib .....................................................\
+..... "
+(git clone https://github.com/siloutil/zlib)\
+ > $prefix/thirdparty/log/zlib-0_download.log 2>&1
+printf "done\n"
 cd zlib
-./configure --static --prefix=$prefix/binaries/zlib
-make
-make test
-make install
+printf "Configuring ........ "
+(./configure --static --prefix=$prefix/thirdparty/zlib)\
+ > $prefix/thirdparty/log/zlib-1_conf.log 2>&1
+printf "building ........ "
+(make -j $proc_make) > $prefix/thirdparty/log/zlib-2_make.log 2>&1
+printf "checking ........ "
+(make -j $proc_make test) > $prefix/thirdparty/log/zlib-3_check.log 2>&1
+printf "installing ....... "
+(make install) > $prefix/thirdparty/log/zlib-4_install.log 2>&1
+printf "done\n"
 cd ..
-
-git clone https://github.com/siloutil/hdf5
-cd hdf5
-./configure --disable-shared --with-zlib=$prefix/binaries/zlib/include,$prefix/binaries/zlib/lib --prefix=$prefix/binaries/hdf5
-make
-make check
-make install
-cd ..
-
-git clone https://github.com/siloutil/silo
-cd silo
-./configure --disable-fortran \
-            --disable-shared \
-            --with-zlib=$prefix/binaries/zlib/include,$prefix/binaries/zlib/lib \
-            --with-hdf5=$prefix/binaries/hdf5/include,$prefix/binaries/hdf5/lib \
-            --prefix=$prefix/binaries/silo
-make
-cd tests
-make check STARGS=DB_HDF5
-cd ..
-make install
-cd ..
-
-mkdir lib64
-mkdir includes
-
-cp binaries/zlib/lib/libz.a lib64/
-
-cp binaries/hdf5/lib/libhdf5.a lib64/
-
-cp binaries/silo/include/silo.h includes/
-cp binaries/silo/include/silo_exports.h includes/
-cp binaries/silo/lib/libsiloh5.a lib64/
-
 rm -rf zlib
+
+## Building hdf5
+printf "Downloading HDF5 .....................................................\
+..... "
+(git clone https://github.com/siloutil/hdf5)\
+ > $prefix/thirdparty/log/hdf5-0_download.log 2>&1
+printf "done\n"
+cd hdf5
+printf "Configuring ........ "
+(./configure --disable-shared\
+  --with-zlib=$prefix/thirdparty/zlib/include,$prefix/thirdparty/zlib/lib\
+  --prefix=$prefix/thirdparty/hdf5
+) > $prefix/thirdparty/log/hdf5-1_conf.log 2>&1
+printf "building ........ "
+(make -j $proc_make) > $prefix/thirdparty/log/hdf5-2_make.log 2>&1
+printf "checking ........ "
+(make -j $proc_make test) > $prefix/thirdparty/log/hdf5-3_check.log 2>&1
+printf "installing ....... "
+(make install) > $prefix/thirdparty/log/hdf5-4_install.log 2>&1
+printf "done\n"
+cd ..
 rm -rf hdf5
+
+## Building silo
+printf "Downloading SILO .....................................................\
+..... "
+(git clone https://github.com/siloutil/silo)\
+ > $prefix/thirdparty/log/silo-0_download.log 2>&1
+cd silo
+printf "Configuring ........ "
+(./configure --disable-fortran\
+  --disable-shared\
+  --with-zlib=$prefix/thirdparty/zlib/include,$prefix/thirdparty/zlib/lib\
+  --with-hdf5=$prefix/thirdparty/hdf5/include,$prefix/thirdparty/hdf5/lib\
+  --prefix=$prefix/thirdparty/silo
+) > $prefix/thirdparty/log/silo-1_conf.log 2>&1
+printf "building ........ "
+(make -j $proc_make) > $prefix/thirdparty/log/silo-2_make.log 2>&1
+printf "checking ........ "
+cd tests
+(make -j $proc_make check STARGS=DB_HDF5) > $prefix/thirdparty/log/silo-3_check.log 2>&1
+cd ..
+printf "installing ....... "
+(make install) > $prefix/thirdparty/log/silo-4_install.log 2>&1
+cd ..
 rm -rf silo
-rm -rf binaries
-) > $logfile 2>&1
+
+## Building boost
+ln -sf $CXX $gcc_dir/gcc/bin/g++-7.3 ## ughh boost
+printf "Downloading boost ....................................................\
+..... "
+(git clone https://github.com/siloutil/boost)\
+ > $prefix/thirdparty/log/boost-0_download.log 2>&1
+printf "done\n"
+cd boost
+printf "Configuring ........ "
+mkdir $prefix/thirdparty/boost
+(./bootstrap.sh --with-toolset=gcc\
+  --libdir=$prefix/thirdparty/boost/lib\
+  --includedir=$prefix/thirdparty/boost/include\
+  --with-libraries=filesystem,python,test
+) > $prefix/thirdparty/log/boost-1_conf.log 2>&1
+printf "building ........ "
+(./b2 install -q\
+  link=static\
+  --libdir=$prefix/thirdparty/boost/lib\
+  --includedir=$prefix/thirdparty/boost/include\
+  --toolset=gcc-7.3
+) > $prefix/thirdparty/log/boost-2_make.log 2>&1
+printf "installing ....... checking ........ "
+(
+echo "Running test for filesystem library"
+cd libs/filesystem/test
+../../../b2 -q --toolset=gcc-7.3
+cd ../../../
+echo "Running test for python library"
+cd libs/python/test
+../../../b2 -q --toolset=gcc-7.3
+cd ../../../
+echo "Running test for test library"
+./b2 headers
+cd libs/test/test
+../../../b2 -q --toolset=gcc-7.3
+cd ../../../
+) > $prefix/thirdparty/log/boost-3_check.log 2>&1
+printf "done\n"
+cd ..
+rm -rf boost
+rm $gcc_dir/gcc/bin/g++-7.3
